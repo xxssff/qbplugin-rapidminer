bc75fdad79a9089f380b13bd7a6e8bebfd8b6767
fjcuberos <fjcuberos@c263dc81-f746-0410-bd36-1f1759a9bfbe>
2008-07-28 11:07:54 +0000

    Comenzando con Kernel

--- trunk/QS/qbts/distances/AbstractExtendedRealValueBasedSimilarity.java
+++ trunk/QS/qbts/distances/AbstractExtendedRealValueBasedSimilarity.java
@@ -9,11 +9,7 @@ import com.rapidminer.operator.similarity.attributebased.AbstractValueBasedSimil
 public abstract class AbstractExtendedRealValueBasedSimilarity extends
 		AbstractValueBasedSimilarity {
 
-	private PreprocessingModel model;
-////SI NO HACE NADA SE ¿PUEDE QUITAR EL METODO COMPLETO? SE SUPONE QUE SE LLAMARA AL PADRE.
-    public void init(ExampleSet exampleSet) throws OperatorException {
-         // Tools.onlyNumericalAttributes(exampleSet, "value based similarities");
-            super.init(exampleSet);
-    }
+	
+
 
 }
