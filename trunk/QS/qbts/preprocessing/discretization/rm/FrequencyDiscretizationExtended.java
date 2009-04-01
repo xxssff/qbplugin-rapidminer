7b0827610e064dad2dacf6a1e5953dba30eb3a1c
fjcuberos <fjcuberos@c263dc81-f746-0410-bd36-1f1759a9bfbe>
2009-04-01 13:35:05 +0000

    Antes de probar FrequencyExtended

--- trunk/QS/qbts/preprocessing/discretization/rm/FrequencyDiscretizationExtended.java
+++ trunk/QS/qbts/preprocessing/discretization/rm/FrequencyDiscretizationExtended.java
@@ -63,172 +63,74 @@ public class FrequencyDiscretizationExtended extends FrequencyDiscretization {
 	public FrequencyDiscretizationExtended(OperatorDescription description) {
 		super(description);
 	}
-
-	public Model createPreprocessingModel(ExampleSet exampleSet) throws OperatorException {
-		
-		if (getParameterAsBoolean(PARAMETER_ALL_ATTRIBUTES)){
-			HashMap<Attribute, double[]> ranges = new HashMap<Attribute, double[]>();
-			// Get and check parametervalues
-			boolean useSqrt = getParameterAsBoolean(PARAMETER_USE_SQRT_OF_EXAMPLES);
-			int numberOfBins = 0;
-			if (!useSqrt) {
-				// if not automatic sizing of bins, use parametervalue
-				numberOfBins = getParameterAsInt(PARAMETER_NUMBER_OF_BINS);
-				if (numberOfBins >= (exampleSet.size() - 1)) {
-					throw new UserError(this, 116, PARAMETER_NUMBER_OF_BINS, "number of bins must be smaller than number of examples (here: " + exampleSet.size() + ")");
-				}
-			} else {
-				exampleSet.recalculateAllAttributeStatistics();
-			}
-
-			// FJ get number of bins
-			if (useSqrt) {
-				numberOfBins=0;
-				for (Attribute currentAttribute : exampleSet.getAttributes()) {
-						numberOfBins = numberOfBins + exampleSet.size() - (int) exampleSet.getStatistics(currentAttribute, Statistics.UNKNOWN);
-				}
-				numberOfBins = (int) Math.round(Math.sqrt(numberOfBins));
-			}
-			
-			List<Double> valores = new ArrayList<Double>();
-			for (Example example : exampleSet) {
-				for (Attribute currentAttribute : exampleSet.getAttributes()) {
-					double value = example.getValue(currentAttribute);
-					if (!Double.isNaN(value)) {
-						valores.add(value);
-					}
-				}
-			}
-			
-			Collections.sort(valores);
-			
-			// finding ranges
-			double examplesPerBin = valores.size() / (double) numberOfBins;
-			double[] attributeRanges = new double[numberOfBins];
-			double currentBinSpace = examplesPerBin;
-			double lastValue = Double.NaN;
-			int currentBin = 0;
-
-			for (double value : valores) {
-				// ** double value = example.getValue(currentAttribute);
-				if (!Double.isNaN(value)) {
-					// change bin if full and not last
-					if (currentBinSpace < 1 && currentBin < numberOfBins && value != lastValue) {
-						if (!Double.isNaN(lastValue)) {
-							attributeRanges[currentBin] = (lastValue + value) / 2;
-							currentBin++;
-							currentBinSpace += examplesPerBin;
-						}
-					}
-					currentBinSpace--;
-					lastValue = value;
-				}
-			}
-			
-
-			
-/*			if (getParameterAsBoolean(PARAMETER_INCLUDE_LIMITS)){
-				
-				// Ampliar attributeRanges por el principio
-				double[] nattRanges=new double[attributeRanges.length+1];
-				// y grabar el valor mínimo
-				attributeRanges[0] = valores.get(0);
-				// y el valor máximo
-				attributeRanges[numberOfBins - 1] = valores.get(valores.size()-1);
-				for(int i=0;i<attributeRanges.length;i++)
-					nattRanges[i+1]=attributeRanges[i];
-				attributeRanges=nattRanges;
-			}
-			else*/
-			
-				attributeRanges[numberOfBins - 1] = Double.POSITIVE_INFINITY;
-
-			// Se asignan los cortes a los atributos 
-			for (Attribute currentAttribute : exampleSet.getAttributes()) {
-				ranges.put(currentAttribute, attributeRanges);
-			}
-			
-			DiscretizationModel model = new DiscretizationModel(exampleSet);
-			model.setRanges(ranges, "range", getParameterAsInt(PARAMETER_RANGE_NAME_TYPE));
-/*			if (getParameterAsBoolean(PARAMETER_INCLUDE_LIMITS)){
-				model.setLimitsIncluded(true);
-			CASO GENERAL
- 				double[][] values = new double[exampleSet.getAttributes().size()][2];
-				int index = 0;
-				for (Attribute attribute : exampleSet.getAttributes()){
-					values[index][0] = valores.get(0);
-					values[index++][1] = valores.get(valores.size()-1);
-				}
-				double[][] values = new double[1][2];
-				values[0][0] = valores.get(0);
-				values[0][1] = valores.get(valores.size()-1);
-				model.setExtremLimits(values);
-			}
-*/				
-			return model;
-		}
-		else{
-			if (getParameterAsBoolean(PARAMETER_INCLUDE_LIMITS)){
-				throw new OperatorException("Extrem limits can´t be included if attributes aren´t discretize altogether.");
-			}
-			DiscretizationModel model=(DiscretizationModel) super.createPreprocessingModel(exampleSet);
-
-			// AMPLIACION PARA INTEGRAR DISCRETIZATIONMODELSERIES CON DISCRETIZATIONMODEL
-			// Ver DiscretizationModelSeries 
-/*
-			DiscretizationModel m = (DiscretizationModel) super.createPreprocessingModel(exampleSet);
-			DiscretizationModelSeries model=new DiscretizationModelSeries(exampleSet,m);
-
-			if (getParameterAsBoolean(PARAMETER_INCLUDE_LIMITS)){
-				model.setLimitsIncluded(true);
-				double[][] values = new double[exampleSet.getAttributes().size()][2];
-				int index = 0;
-				for (Attribute attribute : exampleSet.getAttributes()){
-					values[index][0] = exampleSet.getStatistics(attribute, Statistics.MINIMUM);
-					values[index++][1] =  exampleSet.getStatistics(attribute, Statistics.MAXIMUM);
-				}
-				model.setExtremLimits(values);
-			}
-	*/
-			return model;
-		}
-	}
+	
+	
 	
 	public void computeValues (ExampleSet eSet, List<Attribute> lA, HashMap<Attribute, double[]> ranges ) throws OperatorException{
-		if (useSqrt) {
-			numberOfBins = (int)Math.round(Math.sqrt(exampleSet.size() - (int) exampleSet.getStatistics(currentAttribute, Statistics.UNKNOWN)));
+		
+		// Get and check parametervalues
+		boolean useSqrt = getParameterAsBoolean(PARAMETER_USE_SQRT_OF_EXAMPLES);
+		int numberOfBins = getParameterAsInt(PARAMETER_NUMBER_OF_BINS);
+		if (!useSqrt) {
+			// if not automatic sizing of bins, use parametervalue
+			numberOfBins = getParameterAsInt(PARAMETER_NUMBER_OF_BINS);
+			if (numberOfBins >= (eSet.size() - 1)) {
+				throw new UserError(this, 116, PARAMETER_NUMBER_OF_BINS, "number of bins must be smaller than number of examples (here: " + eSet.size() + ")");
+			}
+		} else {
+			eSet.recalculateAllAttributeStatistics();
 		}
-		double[] attributeRanges = new double[numberOfBins];
-		ExampleSet sortedSet = new SortedExampleSet(exampleSet, currentAttribute, SortedExampleSet.INCREASING);
 
+		if (useSqrt) {
+			for (Attribute currentAttribute : lA) {
+					numberOfBins = numberOfBins + eSet.size() - (int) eSet.getStatistics(currentAttribute, Statistics.UNKNOWN);
+			}
+			numberOfBins = (int) Math.round(Math.sqrt(numberOfBins));
+		}
+
+		List<Double> valores = new ArrayList<Double>();
+		for (Example example : eSet) {
+			for (Attribute currentAttribute : eSet.getAttributes()) {
+				double value = example.getValue(currentAttribute);
+				if (!Double.isNaN(value)) {
+					valores.add(value);
+				}
+			}
+		}
+		
+		Collections.sort(valores);
+		
 		// finding ranges
-		double examplesPerBin = exampleSet.size() / (double) numberOfBins;
+		double examplesPerBin = valores.size() / (double) numberOfBins;
+		double[] attributeRanges = new double[numberOfBins];
 		double currentBinSpace = examplesPerBin;
 		double lastValue = Double.NaN;
 		int currentBin = 0;
 
-		for (Example example : sortedSet) {
-			double value = example.getValue(currentAttribute);
+		for (double value : valores) {
 			if (!Double.isNaN(value)) {
 				// change bin if full and not last
 				if (currentBinSpace < 1 && currentBin < numberOfBins && value != lastValue) {
 					if (!Double.isNaN(lastValue)) {
 						attributeRanges[currentBin] = (lastValue + value) / 2;
 						currentBin++;
-						currentBinSpace += examplesPerBin; //adding because same values might cause binspace to be negative
-						if (currentBinSpace < 1)
-							throw new UserError(this, 944, currentAttribute.getName());
+						currentBinSpace += examplesPerBin;
 					}
 				}
 				currentBinSpace--;
 				lastValue = value;
 			}
 		}
+		
 		attributeRanges[numberOfBins - 1] = Double.POSITIVE_INFINITY;
-		ranges.put(currentAttribute, attributeRanges);
-	}	
-	
 
+		// Se asignan los cortes a los atributos 
+		for (Attribute currentAttribute : eSet.getAttributes()) {
+			ranges.put(currentAttribute, attributeRanges);
+		}
+
+	}
+	
 	public List<ParameterType> getParameterTypes() {
 		List<ParameterType> types = super.getParameterTypes();
 		
@@ -238,6 +140,8 @@ public class FrequencyDiscretizationExtended extends FrequencyDiscretization {
 		type.registerDependencyCondition(new BooleanParameterCondition(this, PARAMETER_USE_SQRT_OF_EXAMPLES, false, false));
 		type.setExpert(false);
 		types.add(type);
+		
+		types.add(new ParameterTypeCategory(PARAMETER_RANGE_NAME_TYPE, "Indicates if long range names including the limits should be used.", DiscretizationModel.RANGE_NAME_TYPES, DiscretizationModel.RANGE_NAME_LONG));
 		
 		return types;
 	}
