143440665284ff9a930cae51f860076cd87b9f1b
fjcuberos <fjcuberos@c263dc81-f746-0410-bd36-1f1759a9bfbe>
2009-04-04 17:19:26 +0000

    Experimento con IntervalKernel funciona. Falta comprobar resultados.

--- QS/com/rapidminer/PluginInit.java
+++ QS/com/rapidminer/PluginInit.java
@@ -6,5 +6,6 @@ import qbts.distances.*;
 public class PluginInit {
   public static void initPlugin(){
 		DistanceMeasures.registerMeasure(DistanceMeasures.NOMINAL_MEASURES_TYPE, "QSISimilarity", QSISimilarity.class);	
+		DistanceMeasures.registerMeasure(DistanceMeasures.NOMINAL_MEASURES_TYPE, "IntervalKernel", IntervalKernel.class);
   }
 }
