2787b97b8910194a80c87b9aedd1bd1bfa3db446
fjcuberos <fjcuberos@c263dc81-f746-0410-bd36-1f1759a9bfbe>
2009-09-01 21:58:44 +0000

    ISOne parametrizable en factor. Falta la parametrización del máximo y mínimo de V1.

---
+++ QS/qbts/distances/DumbOp.java
@@ -0,0 +1,55 @@
+package qbts.distances;
+
+import java.util.List;
+
+import com.rapidminer.example.ExampleSet;
+import com.rapidminer.operator.IOObject;
+import com.rapidminer.operator.InputDescription;
+import com.rapidminer.operator.Model;
+import com.rapidminer.operator.Operator;
+import com.rapidminer.operator.OperatorDescription;
+import com.rapidminer.operator.OperatorException;
+import com.rapidminer.operator.preprocessing.discretization.DiscretizationModel;
+import com.rapidminer.parameter.ParameterType;
+import com.rapidminer.parameter.ParameterTypeCategory;
+import com.rapidminer.parameter.ParameterTypeDouble;
+
+public class DumbOp extends Operator {
+
+	public static final String PARAMETER_P1= "PARAMETER_1";
+	public static final String PARAMETER_P2= "PARAMETER_2";
+	public static final String PARAMETER_P3= "PARAMETER_3";
+	
+	public DumbOp(OperatorDescription description) {
+		super(description);
+	}
+
+	@Override
+	public IOObject[] apply() throws OperatorException {
+		DumbResult db=new DumbResult(getParameterAsDouble(PARAMETER_P1),getParameterAsDouble(PARAMETER_P2),getParameterAsDouble(PARAMETER_P3));
+		
+		return new IOObject[] { db };
+	}
+
+	@Override
+	public Class<?>[] getInputClasses() {
+		return null;
+	}
+
+	@Override
+	public Class<?>[] getOutputClasses() {
+		return new Class[] { DumbResult.class };
+	}
+	
+
+	
+	public List<ParameterType> getParameterTypes() {
+		List<ParameterType> types = super.getParameterTypes();
+		
+		types.add(new ParameterTypeDouble(PARAMETER_P1,"Value of parameter1", Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY,0.0));
+		types.add(new ParameterTypeDouble(PARAMETER_P2,"Value of parameter2", Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY,0.0));
+		types.add(new ParameterTypeDouble(PARAMETER_P3,"Value of parameter3", Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY,0.0));
+		return types;
+	}
+	
+}
--- QS/qbts/distances/ISone.java
+++ QS/qbts/distances/ISone.java
@@ -1,8 +1,54 @@
 package qbts.distances;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
 
-public class ISone extends AbstractIS {
+import com.rapidminer.example.Attribute;
+import com.rapidminer.example.AttributeRole;
+import com.rapidminer.example.ExampleSet;
+import com.rapidminer.operator.IOContainer;
+import com.rapidminer.operator.MissingIOObjectException;
+import com.rapidminer.operator.Model;
+import com.rapidminer.operator.Operator;
+import com.rapidminer.operator.OperatorException;
+import com.rapidminer.operator.preprocessing.discretization.DiscretizationModel;
+import com.rapidminer.parameter.ParameterHandler;
+import com.rapidminer.tools.LogService;
+import com.rapidminer.tools.Ontology;
+import com.rapidminer.tools.container.Tupel;
+import com.rapidminer.tools.math.similarity.SimilarityMeasure;
 
+
+public class ISone extends AbstractIS {  // SimilarityMeasure {
+
+	
+	private double cp = 1;  // punto de cambio que limita las dos funciones cuando d=cp
+	
+	private double MaxValueV1 = 1;
+	private double MinValueV1 = 0;
+	private double fx=0.0; 
+	
+	
+	public void init(ExampleSet exampleSet, ParameterHandler parameterHandler, IOContainer ioContainer) throws OperatorException {
+
+		LogService log = ((Operator) parameterHandler).getLog(); //Local log of calling operator
+		
+		DumbResult dr=null;
+		try {
+			dr = (DumbResult) ioContainer.remove(DumbResult.class);
+		} catch (MissingIOObjectException e1) {
+			log.log("ISOne has not found DumbResult parameters. Using default values factor=0.5", LogService.MAXIMUM);
+			fx = 0.5;
+			return;
+		}
+		
+		fx = dr.getP1();
+		log.log("ISOne has found DumbResult parameters. Using values factor=" + (new Double(fx)).toString(), LogService.MAXIMUM);
+	}
+	
 	public double IS(double c1, double r1, double c2, double r2){
 		double d, R;
 		
@@ -13,16 +59,15 @@ public class ISone extends AbstractIS {
 		
 		d= Math.abs(c1-c2)/(r1+r2);
 		
-		double v1= 0.5 * ( Math.cos(Math.PI * (1-R))+1);
-					// =0,5*(COS(PI()*(1-L2))+1)
+		double v1= (MaxValueV1 - MinValueV1) * 0.5 * ( Math.cos(Math.PI * (1-R))+1) + MinValueV1;
 		
 		if (d==0){
 			return v1;
 		}
-		else if (d < 1) {
-			return v1/2*(Math.sin(Math.PI/2*(1+d))+1);
+		else if (d < cp) {
+			return (v1-fx*v1) * Math.cos(d / cp * Math.PI / 2) + fx*v1;
 		}else{
-			return v1/(2*d);
+			return (fx*v1*cp)/d ; // == fx/(d/cp);
 		}
 	}
 	
