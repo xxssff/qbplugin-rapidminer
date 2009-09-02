6ba12df397b7ed560592e90f3f1ea857511a52fa
fjcuberos <fjcuberos@c263dc81-f746-0410-bd36-1f1759a9bfbe>
2009-09-02 14:36:52 +0000

    ISone y ISTwo parametrizables

--- trunk/QS/com/rapidminer/PluginInit.java
+++ trunk/QS/com/rapidminer/PluginInit.java
@@ -9,8 +9,6 @@ public class PluginInit {
 		DistanceMeasures.registerMeasure(DistanceMeasures.NOMINAL_MEASURES_TYPE, "QSISimilarity", QSISimilarity.class);	
 		DistanceMeasures.registerMeasure(DistanceMeasures.NOMINAL_MEASURES_TYPE, "IntervalKernel", IntervalKernel.class);
 		DistanceMeasures.registerMeasure(DistanceMeasures.NOMINAL_MEASURES_TYPE, "ISone", ISone.class);
-		DistanceMeasures.registerMeasure(DistanceMeasures.NOMINAL_MEASURES_TYPE, "ISone_2", ISone_2.class);
 		DistanceMeasures.registerMeasure(DistanceMeasures.NOMINAL_MEASURES_TYPE, "IS-2", IS2.class);
-		DistanceMeasures.registerMeasure(DistanceMeasures.NOMINAL_MEASURES_TYPE, "IS-2.2", IS2_2.class);
   }
 }
--- trunk/QS/qbts/distances/IS2.java
+++ trunk/QS/qbts/distances/IS2.java
@@ -1,8 +1,43 @@
 package qbts.distances;
 
+import com.rapidminer.example.ExampleSet;
+import com.rapidminer.operator.IOContainer;
+import com.rapidminer.operator.MissingIOObjectException;
+import com.rapidminer.operator.Operator;
+import com.rapidminer.operator.OperatorException;
+import com.rapidminer.parameter.ParameterHandler;
+import com.rapidminer.tools.LogService;
+
 
 public class IS2 extends AbstractIS {
+	
+	private double cp = 1;  // punto de cambio que limita las dos funciones cuando d=cp
+	
+	private double MaxValueV1 = 1;
+	private double MinValueV1 = 0;
+	private double fx=0.5; 
+	
+	
+	public void init(ExampleSet exampleSet, ParameterHandler parameterHandler, IOContainer ioContainer) throws OperatorException {
 
+		LogService log = ((Operator) parameterHandler).getLog(); //Local log of calling operator
+		
+		DumbResult dr=null;
+		try {
+			dr = (DumbResult) ioContainer.remove(DumbResult.class);
+			fx = dr.getP1();
+			MinValueV1 = dr.getP2();
+			MaxValueV1 = dr.getP3();
+			log.log("ISOne has found DumbResult parameters.", LogService.MAXIMUM);
+			log.log("Using values factor=" + (new Double(fx)).toString() + " minV1=" + (new Double(MinValueV1)).toString()+ 
+					" MaxV1=" + (new Double(MaxValueV1)).toString(), LogService.MAXIMUM);
+		} catch (MissingIOObjectException e1) {
+			log.log("ISOne has not found DumbResult parameters. Using default values factor=0.5", LogService.MAXIMUM);
+			return;
+		}
+		
+	}
+	
 	public double IS(double c1, double r1, double c2, double r2){
 		double d, R;
 		
@@ -13,16 +48,15 @@ public class IS2 extends AbstractIS {
 		
 		d= Math.abs(c1-c2)/(r1+r2);
 		
-		double v1= Math.cos(Math.PI/2*(1-R));
-		//  Math.cos(Math.PI/2*(1-R))
-		// COS(PI/2-PI/2*R)
+		double v1=(MaxValueV1-MinValueV1) * Math.cos(Math.PI/2*(1-R))+MinValueV1;
+
 		if (d==0){
 			return v1;
 		}
-		else if (d < 1) {
-			return v1/2*(Math.sin(Math.PI/2*(1+d))+1);
+		else if (d < cp) {
+			return (v1-fx*v1) * Math.cos(d / cp * Math.PI / 2) + fx*v1;
 		}else{
-			return v1/(2*d);
+			return (fx*v1*cp)/d ; // == fx/(d/cp);
 		}
 	}
 	
